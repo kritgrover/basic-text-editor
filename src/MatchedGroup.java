2d8cfc556829bbbee395f89094d102e70d53724d
Krit Grover <105399404+kritgrover@users.noreply.github.com>
2022-05-17 18:21:12 -0400

    Update MatchedGroup.java
    Added comments and code reorganization

--- src/MatchedGroup.java
+++ src/MatchedGroup.java
@@ -1,17 +1,24 @@
+/* MatchedGroup Class
+ * Methods to return Start and End Indexes for given String
+*/
 public class MatchedGroup {
     private final int startIndex;
     private final int endIndex;
-
+    
+    //Constructor to set indexes
     public MatchedGroup(int start, int end) {
         this.startIndex = start;
         this.endIndex = end;
     }
-
-    public int getEndIndex() {
-        return endIndex;
-    }
-
+    
+    //Getter for StartIndex
     public int getStartIndex() {
         return startIndex;
     }
+    
+    //Getter for EndIndex
+    public int getEndIndex() {
+        return endIndex;
+    }
+    
 }
