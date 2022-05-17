a485f78e98c89af472f5d81eec10aa2211912ed2
Krit Grover <105399404+kritgrover@users.noreply.github.com>
2022-05-17 17:47:21 -0400

    Update ApplicationRunner.java
    Added comments

--- src/ApplicationRunner.java
+++ src/ApplicationRunner.java
@@ -1,3 +1,8 @@
+/* Runner Class
+* Contains main method for the application to run
+* Use this to start the app
+*/
+
 public class ApplicationRunner {
     public static void main(String[] args) {
         new TextEditor();
